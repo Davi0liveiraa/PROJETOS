a0c131091a45a6199fd0f5c453d61131daf083b6
Davi0liveiraa <rdavioliveira2020@gmail.com>
2024-05-14 15:00:53 -0300

    comentario

--- ExerciciosNovos/ex01.py
+++ ExerciciosNovos/ex01.py
@@ -1,3 +1,4 @@
 '''
      Crie um programa que leia quanto dinheiro em reais uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
 '''
+real = float(input("Quanto reais você tem na carteira? "))
